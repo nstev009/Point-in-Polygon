fd39dd865c776959a938dd620a43313e9442b201
Jean Philippe Wan <jean.philippe.wan@statcan.gc.ca>
2024-05-08 11:40:09 -0400

    cleaned up code

--- run_pip_sqlalchemy.py
+++ run_pip_sqlalchemy.py
@@ -4,17 +4,16 @@ Created on Tue Jul  4 15:25:51 2023
 
 @author: wjeanph
 """
-# import cx_Oracle
-import geopandas as gpd
-import pandas as pd
-from shapely.geometry import Polygon, MultiPolygon, Point
-from sqlalchemy import create_engine
-from sqlalchemy.pool import QueuePool
-from pyproj import CRS
-import keyring as kr
-
 import time
 
+# import cx_Oracle
+import geopandas as gpd
+import keyring as kr
+import pandas as pd
+from pyproj import CRS
+from shapely.geometry import MultiPolygon, Point, Polygon
+from sqlalchemy import create_engine
+from sqlalchemy.pool import QueuePool
 
 ########################################################################################################################
 ########################################################################################################################
@@ -32,20 +31,23 @@ spatial_reference = 3347
 conditions = None  # Optional, set to None if not needed
 sample = False
 
-csv_long_lat_file = 'C:/Users/wjeanph/Documents/python/point_in_poly/coordinates/poc_20230301_latlong_48b.csv'
-output_csv_file =  'C:/Users/wjeanph/Documents/python/point_in_poly/coordinates/joined_poc_20230301_latlong_48b.csv'
+csv_long_lat_file = "C:/Users/wjeanph/Documents/python/point_in_poly/coordinates/poc_20230301_latlong_48b.csv"
+output_csv_file = "C:/Users/wjeanph/Documents/python/point_in_poly/coordinates/joined_poc_20230301_latlong_48b.csv"
 
 
 # Specify the connection credentials
 cred = kr.get_credential(hostname, "")
 username = cred.username
 password = cred.password
+print(username)
+print(password)
 
 # Specify the connection string
 connection_str = f"oracle+cx_oracle://{username}:{password}@{hostname}"
 
 ########################################################################################################################
 ########################################################################################################################
+
 
 class Timer:
     def __init__(self):
@@ -60,27 +62,29 @@ class Timer:
         time_lapse_minutes = time_lapse / 60.0
         print(f"Time lapse: {time_lapse_minutes:.2f} minutes")
 
+
 def time_it(func):
     def wrapper(*args, **kwargs):
         start_time = time.time()
         result = func(*args, **kwargs)
         end_time = time.time()
-        execution_time = (end_time - start_time)/60
+        execution_time = (end_time - start_time) / 60
         print(f"Execution time for {func.__name__}: {execution_time:.2f} minutes")
         return result
+
     return wrapper
 
 
 def process_polygon_string(polygon_string):
     """Converts a polygon string to a Shapely MultiPolygon object.
-    
+
     Args:
         polygon_string (str): The polygon string to process.
-        
+
     Returns:
         Polygon or MultiPolygon: The processed Polygon or MultiPolygon object.
     """
-    # Remove the outer "POLYGON ((" and "))" parts from the 
+    # Remove the outer "POLYGON ((" and "))" parts from the
     # print(type(polygon_string))
     # print(polygon_string)
     polygon_string = polygon_string.replace("POLYGON ((", "").replace("))", "")
@@ -92,7 +96,9 @@ def process_polygon_string(polygon_string):
     polygons = []
     for sub_polygon_string in sub_polygon_strings:
         # Split the sub-polygon string by commas and spaces to get individual coordinates
-        coordinates = [tuple(map(float, coord.split())) for coord in sub_polygon_string.split(", ")]
+        coordinates = [
+            tuple(map(float, coord.split())) for coord in sub_polygon_string.split(", ")
+        ]
         # Create the Shapely Polygon object and add it to the list
         polygons.append(Polygon(coordinates))
 
@@ -104,10 +110,20 @@ def process_polygon_string(polygon_string):
 
     return multipolygon
 
+
 @time_it
-def query_database_and_get_dataframe(connection_str, database_name, table_name, uid, shape_column, spatial_reference, conditions=None, sample=False):
+def query_database_and_get_dataframe(
+    connection_str,
+    database_name,
+    table_name,
+    uid,
+    shape_column,
+    spatial_reference,
+    conditions=None,
+    sample=False,
+):
     """Queries the database and retrieves a GeoPandas DataFrame.
-    
+
     Args:
         connection_str (str): The Oracle database connection string.
         database_name (str): The name of the database.
@@ -117,13 +133,13 @@ def query_database_and_get_dataframe(connection_str, database_name, table_name, 
         spatial_reference (str): The spatial reference system of the data.
         conditions (str, optional): Additional query conditions. Defaults to None.
         sample (bool, optional): Flag indicating whether to sample the data. Defaults to False.
-        
+
     Returns:
         gpd.GeoDataFrame: The resulting GeoPandas DataFrame.
     """
     # Create a SQLAlchemy engine with connection pooling
     engine = create_engine(connection_str, poolclass=QueuePool)
-    
+
     # Construct the SQL query
     query = f"SELECT {uid}, SDE.ST_asText({shape_column}) as GEOMETRY FROM {database_name}.{table_name}"
 
@@ -139,7 +155,7 @@ def query_database_and_get_dataframe(connection_str, database_name, table_name, 
         query += "ROWNUM <= 100"
 
     timer = Timer()
-    
+
     # Read the SQL query result into a DataFrame
     print("Querying databse")
     timer.start()
@@ -148,22 +164,31 @@ def query_database_and_get_dataframe(connection_str, database_name, table_name, 
 
     print("Converting shape to shapely geometry")
     timer.start()
-    df['geometry'] = df['geometry'].apply(process_polygon_string)
+    df["geometry"] = df["geometry"].apply(process_polygon_string)
     timer.end()
-    
-    
-    gpdf = gpd.GeoDataFrame(df, geometry='geometry')
-    
- 
-    gpdf.geometry.set_crs(crs = CRS.from_epsg(spatial_reference), inplace =True)
-    
+
+    gpdf = gpd.GeoDataFrame(df, geometry="geometry")
+
+    gpdf.geometry.set_crs(crs=CRS.from_epsg(spatial_reference), inplace=True)
+
     return gpdf
+
 
 # Call the function to query the database and get the GeoPandas DataFrame
 
-df_shapes = query_database_and_get_dataframe(connection_str, database_name, table_name, uid, shape_column, spatial_reference, conditions, sample)
-df_shapes.to_csv(r'bb_uid_shape2.csv')
+df_shapes = query_database_and_get_dataframe(
+    connection_str,
+    database_name,
+    table_name,
+    uid,
+    shape_column,
+    spatial_reference,
+    conditions,
+    sample,
+)
+df_shapes.to_csv(r"bb_uid_shape2.csv")
 # df_shapes.geometry.set_crs(spatial_reference, inplace =True)
+
 
 def get_object_size_in_gb(obj):
     """
@@ -183,17 +208,18 @@ def get_object_size_in_gb(obj):
 
     return size_gb
 
+
 get_object_size_in_gb(df_shapes)
 
 print(df_shapes)
 
 
-df = pd.read_csv(csv_long_lat_file, names = ["unique_identifier", "latitude", "longitude"])
+df = pd.read_csv(
+    csv_long_lat_file, names=["unique_identifier", "latitude", "longitude"]
+)
 
 geometry = [Point(xy) for xy in zip(df.longitude, df.latitude)]
 # df_points = gpd.GeoDataFrame(df, geometry=geometry)
-
-
 
 
 # Specify the CRS (Coordinate Reference System) using EPSG code 4326
@@ -205,7 +231,7 @@ print(df_points)
 
 print("Converting from epsg 4326 to epsg 3347")
 target_crs = CRS.from_epsg(3347)
-df_points.to_crs(target_crs, inplace = True)
+df_points.to_crs(target_crs, inplace=True)
 print(df_points)
 
 # df_points.geometry.set_crs(spatial_reference, inplace =True)
@@ -220,8 +246,3 @@ df_points.plot()
 merged_gdf = gpd.sjoin(df_points, df_shapes, how="inner", predicate="within")
 print(merged_gdf.head())
 merged_gdf.to_csv(output_csv_file)
-
-
-
-
-
